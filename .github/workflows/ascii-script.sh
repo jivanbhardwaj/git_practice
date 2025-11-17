71c8244c05168ea2be8607c275e88b26300be355
JIVAN BHARDWAJ <jivan.6b@gmail.com>
2025-11-17 17:09:32 +0530

    Executing Shell Scripts in Workflow

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,6 @@
+    #/bin/bash
+    ls -ltrh
+    sudo apt-get update && sudo apt-get install -y cowsay
+    cowsay -f dragon "Run for art work, and save it to dragon.txt" >> dragon.txt
+    grep -inr "dragon.txt"
+    cat dragon.txt
