666830522b581b2f72d78e7140d1aaee0d97a130
JIVAN BHARDWAJ <jivan.6b@gmail.com>
2025-11-17 17:26:05 +0530

    execute the shell script

--- .github/workflows/bash.sh
+++ .github/workflows/bash.sh
@@ -1,4 +1,4 @@
-    #/bin/bash
+    #/bin/sh
     ls -ltrh
     sudo apt-get update && sudo apt-get install -y cowsay
     cowsay -f dragon "Run for art work, and save it to dragon.txt" >> dragon.txt
